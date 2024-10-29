0091e93469bf1cdf2f10f277d6be5b8317a44208
Thomazrlima <thomazlima211@gmail.com>
2024-10-29 20:00:53 -0300

    Atividade de BD

--- BD/Códigos/UniversidadeConsulta.sql
+++ BD/Códigos/UniversidadeConsulta.sql
@@ -56,5 +56,105 @@ from ministra m
 join professor prof on m.matricula_professor = prof.matricula_professor
 join pessoa p on prof.matricula_professor = p.matricula_pessoa
 group by p.nome, prof.data_admissao
-having COUNT(m.codigo_disciplina) > 4;
+having count(m.codigo_disciplina) > 4;
 
+-- 8
+select m.matricula_aluno, p.nome, m.ano_semestre,
+       avg(pr.nota) as nota_final
+from monitoria m
+join pessoa p on m.matricula_aluno = p.matricula_pessoa
+join prova pr on pr.matricula_aluno = m.matricula_aluno and pr.codigo_disciplina = m.codigo_disciplina
+where m.codigo_disciplina = 3
+group by m.matricula_aluno, p.nome, m.ano_semestre;
+
+-- 9
+select count(distinct matricula_aluno) as qtd_alunos
+from aluno_turma
+where ano_semestre = '2010.1' and codigo_projeto is not null;
+
+-- 10
+select matricula_aluno, count(*) as qtd_monitorias
+from monitoria
+group by matricula_aluno;
+
+-- 11
+select a.matricula_aluno, p.nome, proj.titulo as nome_projeto
+from aluno_turma at
+join aluno a on at.matricula_aluno = a.matricula_aluno
+join pessoa p on a.matricula_aluno = p.matricula_pessoa
+join projeto proj on at.codigo_projeto = proj.codigo_projeto
+where proj.conceito = 'regular';
+
+-- 12
+select distinct pa.nome as nome_aluno, pp.nome as nome_professor
+from prova pr
+join pessoa pa on pr.matricula_aluno = pa.matricula_pessoa
+join ministra m on pr.codigo_disciplina = m.codigo_disciplina and pr.codigo_curso = m.codigo_curso and pr.ano_semestre = m.ano_semestre
+join professor prof on m.matricula_professor = prof.matricula_professor
+join pessoa pp on prof.matricula_professor = pp.matricula_pessoa
+where pr.descricao = 'final';
+
+-- 13
+select ano_semestre, count(*) as qtd_alunos
+from aluno_turma
+where codigo_disciplina = 2
+group by ano_semestre;
+
+-- 14
+select c.nome
+from curso c
+left join aluno_turma at on c.codigo_curso = at.codigo_curso and at.codigo_projeto is not null
+where at.codigo_projeto is null;
+
+-- 15
+select ano_semestre, count(distinct matricula_aluno) as qtd_alunos
+from aluno_turma
+group by ano_semestre;
+
+-- 16
+select p.nome
+from aluno a
+join pessoa p on a.matricula_aluno = p.matricula_pessoa
+join aluno_turma at on a.matricula_aluno = at.matricula_aluno
+join disciplina d on at.codigo_disciplina = d.codigo_disciplina
+where p.sexo = 'f' and d.ementa like '%estatística%';
+
+-- 17
+select pp.nome as nome_professor, count(distinct m.matricula_aluno) as qtd_monitores
+from monitoria m
+join professor prof on m.matricula_professor = prof.matricula_professor
+join pessoa pp on prof.matricula_professor = pp.matricula_pessoa
+join disciplina d on m.codigo_disciplina = d.codigo_disciplina
+where d.ementa like '%matemática%'
+group by pp.nome;
+
+-- 18
+select prof.matricula_professor, p.nome, prof.data_admissao, count(distinct m.codigo_disciplina) as qtd_disciplinas
+from ministra m
+join professor prof on m.matricula_professor = prof.matricula_professor
+join pessoa p on prof.matricula_professor = p.matricula_pessoa
+where m.ano_semestre like '2009.%'
+group by prof.matricula_professor, p.nome, prof.data_admissao
+having count(distinct m.codigo_disciplina) > 1;
+
+-- 19
+select distinct a.matricula_aluno, p.nome
+from monitoria m
+join aluno a on m.matricula_aluno = a.matricula_aluno
+join pessoa p on a.matricula_aluno = p.matricula_pessoa;
+
+-- 20
+select c.nome as nome_curso, a.matricula_aluno, a.nota_vestibular
+from aluno a
+join curso c on a.codigo_curso = c.codigo_curso
+where (a.codigo_curso, a.nota_vestibular) in (
+    select codigo_curso, max(nota_vestibular)
+    from aluno
+    group by codigo_curso
+);
+
+-- 21
+select p.nome, m.matricula_professor
+from monitoria m
+join pessoa p on m.matricula_aluno = p.matricula_pessoa
+where p.nome like 'A%';
